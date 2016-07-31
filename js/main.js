ff7fc4db06356d36117317ee177a0d66515d7c60
dyehay <dyehay@gmail.com>
2016-07-31 14:12:51 +0930

    more more styling

--- js/main.js
+++ js/main.js
@@ -11,6 +11,8 @@ app.run(function($transform) {
 
 app.config(function($routeProvider) {
   $routeProvider.when('/',              {templateUrl: 'pages/launch.html', reloadOnSearch: false});
+  $routeProvider.when('/intro1',        {templateUrl: 'pages/launch1.html', reloadOnSearch: false});
+  $routeProvider.when('/intro2',        {templateUrl: 'pages/launch2.html', reloadOnSearch: false});
   $routeProvider.when('/start',         {templateUrl: 'pages/start.html', reloadOnSearch: false});
   $routeProvider.when('/options',       {templateUrl: 'pages/options.html', reloadOnSearch: false});
   $routeProvider.when('/map',           {templateUrl: 'pages/map.html', reloadOnSearch: false});
